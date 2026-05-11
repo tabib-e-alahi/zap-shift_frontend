16e99a82f803e4126e6a98935065af4c0edfe899
tabib-e-alahi <etabib.alahi@gmail.com>
2026-05-11 23:00:00 +0600

    first commit

---
+++ src/routes/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+export default router;
