a50fdd98e3e2c5de02ad6f78b8dcb2bf7b1f9052
tabib-e-alahi <etabib.alahi@gmail.com>
2026-05-16 23:00:00 +0600

    navbar added

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router';
+import Footer from '../pages/Shared/Footer/Footer';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default RootLayout;
