915c045162e62577b86d5c68af69e15e63d9107d
tabib-e-alahi <etabib.alahi@gmail.com>
2026-05-17 23:00:00 +0600

    navbar added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home/Home";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
   },
 ]);
 
